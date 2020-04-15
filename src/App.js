b26d49290f65e46bf7df674589228b504b80d305
cnnrbrn <connorbobrien@gmail.com>
2020-04-15 17:42:13 +0200

    Add snap scrolling

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import React, { useRef } from "react";
 import "./App.css";
 
-const BackToTop = function () {
+const BackToTop = function ({ elementRef }) {
+    console.log("ref", elementRef);
+
     function ScrollToTop() {
-        window.scrollTo({ behavior: "smooth", top: 0 });
+        elementRef.current.scrollTo({ behavior: "smooth", top: 0 });
     }
 
     return <button onClick={ScrollToTop}>Go to Top</button>;
 };
 
 function App() {
+    const refApp = useRef(null);
     const refB = useRef(null);
     const refC = useRef(null);
 
-    function scrollToSection(ref) {
-        window.scrollTo({ behavior: "smooth", top: ref.current.offsetTop });
+    function scrollToSection(appRef, sectionRef) {
+        appRef.current.scrollTo({ behavior: "smooth", top: sectionRef.current.offsetTop });
     }
 
     return (
-        <div className="App">
-            <button onClick={() => scrollToSection(refB)}>Go to section B</button>
-            <button onClick={() => scrollToSection(refC)}>Go to section C</button>
+        <div className="App" ref={refApp}>
+            <button onClick={() => scrollToSection(refApp, refB)}>Go to section B</button>
+            <button onClick={() => scrollToSection(refApp, refC)}>Go to section C</button>
 
             <section>Section A</section>
 
             <section ref={refB}>
                 Section B
-                <BackToTop />
+                <BackToTop elementRef={refApp} />
             </section>
             <section ref={refC}>
                 Section C
-                <BackToTop />
+                <BackToTop elementRef={refApp} />
             </section>
         </div>
     );
